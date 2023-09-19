f1ba613127a0f69996df41d525f17eccedb41eb6
Alv <72032190+alvwashere@users.noreply.github.com>
2023-09-19 03:37:30 +0100

    Update config.lua
    Add Anim Configs + Job Configs
    
    V1.0.3 feature but added to config early: RESTRICTED TO

--- config.lua
+++ config.lua
@@ -8,14 +8,26 @@ Config.RepairLocations = {
         Location = vec3(-97.6829, -1791.9990, 26.9096), -- The coordinates for the bench (vector3)
         Label = 'LCF Bench', -- Label for the location you're at.
         Jobs = {
-            'mechanic', -- Job required to operate the table, feel free to add more by adding a new line.
-        }
+            ['mechanic'] = true, -- Job required to operate the table, feel free to add more by adding a new line.
+        },
+        RestrictedTo = { -- Remove the table and set this to false to allow every gun on the table
+            ['WEAPON_COMBATPISTOL'] = true,
+            ['WEAPON_CARBINERIFLE'] = true,
+            ['WEAPON_PISTOL'] = true
+        },
     }
 }
 
 Config.Repairing = {
     InstantRepair = false, -- Do you want the weapon to repair instantly? Setting to false will use a progress bar and optional animation.
-
+    Fixing = {
+        AnimDict = 'anim@heists@prison_heiststation@cop_reactions',
+        AnimClip = 'cop_b_idle'
+    },
+    Cleaning = {
+        AnimDict = 'timetable@floyd@clean_kitchen@base',
+        AnimClip = 'base'
+    },
 }
 
 Config.Requirements = { -- The amount of metal required to fix a gun based on its durability.
