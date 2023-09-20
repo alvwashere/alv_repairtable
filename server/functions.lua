625668ddfc1ff57098ff8accc9c46a6ed2ccd4c4
Alv <72032190+alvwashere@users.noreply.github.com>
2023-09-21 00:17:13 +0100

    Update functions.lua
    Add discord log function for next update, will be applied with a server sided configuration file as well as updates to actual server sided code soon ready for update V1.0.4.

--- server/functions.lua
+++ server/functions.lua
@@ -11,3 +11,28 @@ function GetJob(source)
     return player.getGroup() -- Haven't used OX Core too much before, seeing from documentation that police and sheriff were stored as a group so that's what I'll assume is the job.
   end
 end
+
+function DiscordLog(webhook, title, message, data)
+  if GetResourceState('alv_lib') == 'missing' then
+    local color, userName, avatarUrl
+  
+    if data.color then color = data.color else color = 16711680 end
+    if data.username then userName = data.username else username = 'Alv.gg' end
+    if data.avatarUrl then avatarUrl = data.avatarUrl else avatarUrl = 'https://ih1.redbubble.net/image.971270805.6405/gbrf,6x6,f,540x540-pad,450x450,f8f8f8.jpg' end
+
+    local embed = {
+      {
+        ['color'] = color,
+        ['title'] = title,
+        ['description'] = message,
+        ['footer'] = {
+          ['text'] = 'Alv.gg - 2023'
+        }
+      }
+    }
+
+    PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username=userName, avatar_url = avatarUrl, embeds = embed}), {['Content-Type'] = 'application/json'})
+  else
+    exports['alv_lib']:SendLog(webhook, title, message)
+  end
+end)
