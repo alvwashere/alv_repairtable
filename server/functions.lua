da4a3ad693cebf30678f6d1576b74011615fab82
Alv <72032190+alvwashere@users.noreply.github.com>
2023-09-20 20:31:28 +0100

    Update functions.lua

--- server/functions.lua
+++ server/functions.lua
@@ -1,5 +1,3 @@
-if not ox_inventory then local ox_inventory = exports.ox_inventory end
-
 function GetJob(source)
   if ESX then
     return ESX.GetPlayerFromId(source).getJob().name
@@ -12,18 +10,4 @@ function GetJob(source)
 
     return player.getGroup() -- Haven't used OX Core too much before, seeing from documentation that police and sheriff were stored as a group so that's what I'll assume is the job.
   end
-end
-
-function GetLoadout(source)
-  return ox_inventory:GetInventoryItems(source)
-end
-
-function GetItem(source)
-  local item = ox_inventory:GetItem(source, Config.MetalItem, nil, true)
-
-  return item
-end
-
-function RemoveItem(source, count)
-  ox_inventory:RemoveItem(source, Config.MetalItem, count)
 end
