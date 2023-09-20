3e8ee3c644b3ef8e76b386c5691fab5c3af35cdf
Alv <72032190+alvwashere@users.noreply.github.com>
2023-09-20 14:02:08 +0100

    Update functions.lua
    Add two new functions for getting loadout and getting metal item, these functions have no code within them just a comment to remind me to come back later to them.

--- server/functions.lua
+++ server/functions.lua
@@ -11,3 +11,11 @@ function GetJob(source)
     return player.getGroup() -- Haven't used OX Core too much before, seeing from documentation that police and sheriff were stored as a group so that's what I'll assume is the job.
   end
 end
+
+function GetLoadout(source)
+  -- no longer need framework support as the script only support ox_inventory, may as well just check count using ox_inventory functions (to follow)
+end
+
+function GetItem(source)
+  -- no longer need framework support as the script only support ox_inventory, may as well just check count using ox_inventory functions (to follow)
+end
