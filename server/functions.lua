21411c5e9e0a30db5597524cbcf8a9b1df4c8071
Alv <72032190+alvwashere@users.noreply.github.com>
2023-09-29 14:28:28 +0100

    Update functions.lua
    add ChargePlayer function...

--- server/functions.lua
+++ server/functions.lua
@@ -40,3 +40,27 @@ function DiscordLog(webhook, title, message, data)
     })
   end
 end)
+
+function ChargePlayer(source, amount)
+  if ESX then
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if xPlayer.getMoney() >= amount then
+      xPlayer.removeMoney(amount)
+      return true
+    else
+      return false
+    end
+  elseif QBCore then
+    local Player = QBCore.Functions.GetPlayer(source)
+
+    if Player.PlayerData.money['cash'] >= amount then
+      Player.Functions.RemoveMoney('cash', amount, locale('repair_bench')
+      return true
+    else
+      return false
+    end
+  elseif OX then
+    print('^6[ALV-GG]^0 OX Core does not currently have a documented money system. This function will be updated when the framework adds one.')
+  end
+end
