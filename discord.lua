344a4a98b2f7c8e7c5e24ee7bc24bef36d36e625
Alv <72032190+alvwashere@users.noreply.github.com>
2023-09-29 13:07:14 +0100

    Update discord.lua
    Add config for Repair Webhook

--- discord.lua
+++ discord.lua
@@ -3,3 +3,5 @@ Discord = {}
 Discord.Color = 16711680,
 Discord.Username = 'Alv.gg'
 Discord.AvatarUrl = 'https://ih1.redbubble.net/image.971270805.6405/gbrf,6x6,f,540x540-pad,450x450,f8f8f8.jpg'
+
+Discord.RepairWebhook = ''
