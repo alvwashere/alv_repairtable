83009fefdcc824f9b9afe6866cec928db718d02a
Alv <72032190+alvwashere@users.noreply.github.com>
2023-10-09 15:46:51 +0100

    Update script.js
    create listener templates for UI + actual code for notification

--- html/script.js
+++ html/script.js
@@ -1,1 +1,16 @@
+window.addEventListener('message', function(event) {
+  if (event.data.type === 'notification') {
+    var notification = document.getElementById('notification');
 
+    notification.style.display = 'block';
+    notification.innerHTML = event.data.data.message;
+
+    sleep(5000).then(() => {
+      notification.style.display = 'none';
+    });
+  } else if (event.data.type === 'openui') {
+
+  } else if (event.data.type === 'closeui') {
+
+  }
+}
