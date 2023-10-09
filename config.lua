a9771c5a2665dbdc2c29df6578fef8c7e672d1c0
Alv <72032190+alvwashere@users.noreply.github.com>
2023-10-09 15:51:51 +0100

    Update config.lua
    Add TimeEach Config meaning you can choose how long it takes to repair a weapon.

--- config.lua
+++ config.lua
@@ -14,7 +14,7 @@ Config.Framework = {
 Config.RepairLocations = {
     [1] = {
         Location = vec3(-97.6829, -1791.9990, 26.9096), -- The coordinates for the bench (vector3)
-        Label = 'LCF Bench', -- Label for the location you're at.
+        Label = 'Grove Street Customs Bench', -- Label for the location you're at.
         Jobs = {
             ['mechanic'] = true, -- Job required to operate the table, feel free to add more by adding a new line.
         },
@@ -29,6 +29,7 @@ Config.RestrictedTo = { -- Remove the table and set this to false to allow every
 
 Config.Repairing = {
     InstantRepair = false, -- Do you want the weapon to repair instantly? Setting to false will use a progress bar and optional animation.
+    TimeEach = 5, -- How long each stage should take in seconds, default is 5 seconds meaning it takes 10 seconds to repeir a weapon.
     Fixing = {
         AnimDict = 'anim@heists@prison_heiststation@cop_reactions',
         AnimClip = 'cop_b_idle'
