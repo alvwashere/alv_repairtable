e5a305ec1a50ab1005f8a344b7bcfb61893a6b27
Alv <72032190+alvwashere@users.noreply.github.com>
2023-10-10 21:58:25 +0100

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,3 +1,5 @@
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
 window.addEventListener('message', function(event) {
   if (event.data.type === 'notification') {
     var notification = document.getElementById('notification');
@@ -9,8 +11,16 @@ window.addEventListener('message', function(event) {
       notification.style.display = 'none';
     });
   } else if (event.data.type === 'openui') {
+    if (event.data.data.weapons) {
+      var ui = document.getElementById('ui_main');
+      ui.style.display = 'block';
+      var ui_header = document.getElementById('ui_header');
+      ui_header.innerHTML = event.data.data.title
 
+      // Need to insert the elements as buttons into the table...
+    }
   } else if (event.data.type === 'closeui') {
-
+    var ui = document.getElementById('ui_main');
+    ui.style.display = 'none';
   }
-}
+});
