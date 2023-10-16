b49f26908e0bb351567042b3ebedce8fbd2cbc76
Alv <72032190+alvwashere@users.noreply.github.com>
2023-10-16 13:17:03 +0100

    Update discord.lua

--- discord.lua
+++ discord.lua
@@ -2,6 +2,6 @@ Discord = {}
 
 Discord.Color = 16711680,
 Discord.Username = 'Alv.gg'
-Discord.AvatarUrl = 'https://ih1.redbubble.net/image.971270805.6405/gbrf,6x6,f,540x540-pad,450x450,f8f8f8.jpg'
+Discord.AvatarUrl = 'https://avatars.githubusercontent.com/u/72032190?v=4'
 
 Discord.RepairWebhook = ''
