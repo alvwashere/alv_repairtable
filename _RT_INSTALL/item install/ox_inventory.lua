24da07b3d364b622adfbf0fd8d49a7ce83c3b8ed
Alv <72032190+alvwashere@users.noreply.github.com>
2024-01-27 19:03:19 +0000

    Add files via upload

---
+++ _RT_INSTALL/item install/ox_inventory.lua
@@ -0,0 +1,10 @@
+    ['gun_table'] = {
+        label = 'Gun Table',
+        weight = 10000,
+        stack = true,
+        close = true,
+        description = "Portable table to fix-up on the go...",
+        client = {
+            event = 'alv_repairtable:placeTable'
+        }
+    },
