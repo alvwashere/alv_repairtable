a47b27623d7ede4a67f1f1d60a47dda705e29b9a
Alv <72032190+alvwashere@users.noreply.github.com>
2024-01-27 19:00:25 +0000

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -22,9 +22,11 @@ for k, v in pairs(Config.RepairLocations) do
                 options = {
                     {
                         name = 'alv_repairbench',
-                        command = 'repair_bench',
                         icon = Config.Framework.Icon,
                         label = v.Label,
+                        onSelect = function()
+                            repairBench()
+                        end
                     }
                 }
             })
@@ -42,23 +44,25 @@ for k, v in pairs(Config.RepairLocations) do
     end
 end
 
-RegisterCommand('repair_bench', function()
+function repairBench(portable)
     local failAttempts = 0
     local Weapons = {}
     local location = nil
 
-    for k, v in pairs(Config.RepairLocations) do
-        if #(GetEntityCoords(cache.ped) - v.Location) > 3.0 then
-            failAttempts = failAttempts + 1
-            location = v.Label
+    if not portable then
+        for k, v in pairs(Config.RepairLocations) do
+            if #(GetEntityCoords(cache.ped) - v.Location) > 3.0 then
+                failAttempts = failAttempts + 1
+                location = v.Label
 
-            if failAttempts >= #Config.RepairLocations then
-                return
+                if failAttempts >= #Config.RepairLocations then
+                    return
+                end
             end
         end
     end
 
-    local allowed = lib.callback.await('alv_repairtable:canUse', false)
+    local allowed = portable or lib.callback.await('alv_repairtable:canUse', false)
 
     if allowed then
         local loadout = lib.callback.await('alv_repairtable:getLoadout', false)
@@ -83,11 +87,93 @@ RegisterCommand('repair_bench', function()
     else
         Config.Notify(locale('no_job'))
     end
-end)
+end
+
+if Config.Command and Config.KeyMapping.Enabled then
+    RegisterCommand('repair_bench', function()
+        repairBench()
+    end)
+end
 
 if Config.KeyMapping.Enabled then
     RegisterKeyMapping('repair_bench', Config.KeyMapping.Description, 'keyboard', Config.Keymapping.Keybind)
 end
+
+RegisterNetEvent('alv_repairtable:placeTable', function()
+    if Config.TableItem.RequireMetalToPlace then
+        local hasMetal = lib.callback.await('alv_repairtable:getMetal', false)
+
+        if hasMetal == 0 then
+            return Config.Notify(locale('no_metal_placing'))
+        end
+    end
+
+    if Config.TableItem.Job then
+        local allowed = lib.callback.await('alv_repairtable:canPlace', false)
+
+        if not allowed then return Config.Notify(locale('no_job_placing')) end
+    end
+
+    if Config.TableItem.TakeOnUse then
+        lib.callback.await('alv_repairtable:takeTable', false, 'place')
+    end
+
+    local tableHash = GetHashKey(Config.TableItem.Prop)
+    RequestModel(tableHash)
+
+    while not HasModelLoaded(tableHash) do
+        Wait(0)
+    end
+
+    local x, y, z = table.unpack(GetEntityCoords(cache.ped))
+    local table = CreateObject(tableHash, x + 2.0, y, z, true, true, true)
+    PlaceObjectOnGroundProperly(table)
+
+    function DeleteTable()
+        SetEntityAsMissionEntity(table)
+        DeleteObject(table)
+    end
+
+    local table_point = lib.points.new({
+        coords = GetEntityCoords(table),
+        distance = 2.0, 
+        dunake = 'nerd',
+    })
+
+    function table_point:onEnter()
+        portable_table = exports.ox_target:addSphereZone({
+            coords = GetEntityCoords(table),
+            radius = Config.Framework.Radius,
+            debug = Config.Debug,
+            options = {
+                {
+                    name = 'alv_useBench',
+                    label = locale('use_repair_bench'), 
+                    icon = Config.Framework.Icon,
+                    onSelect = function()
+                        repairBench(true)
+                    end
+                },
+                {
+                    name = 'alv_takeBench',
+                    label = locale('pick_bench_up'), 
+                    icon = Config.Framework.Icon,
+                    onSelect = function()
+                        DeleteTable()
+
+                        if Config.TableItem.TakeOnUse then
+                            lib.callback.await('alv_repairtable:takeTable', false, 'pickup')
+                        end
+                    end
+                },
+            }
+        })
+    end
+
+    function table_point:onExit()
+        exports.ox_target:removeZone(portable_table)
+    end
+end)
 
 AddEventHandler('onResourceStop', function(name)
     if name == GetCurrentResourceName() then
